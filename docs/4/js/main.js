ebb41304b3e65de6afc7d9bfad6bf18a5dd3d576
ytyaru <yttry0@gmail.com>
2025-07-06 22:26:20 +0900

    add

--- docs/4/js/main.js
+++ docs/4/js/main.js
@@ -1,20 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOMContentLoaded!!');
-    /*
-    const author = 'ytyaru';
-    van.add(document.querySelector('main'), 
-        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.PaintEditor.20250706073455/`}, 'PaintEditor')),
-        van.tags.p('ラスタ画像を描画する簡易エディタ。'),
-//        van.tags.p('A simple editor for drawing raster images.'),
-    );
-    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
-
-    const a = new Assertion();
-    a.t(true);
-    a.f(false);
-    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
-    a.fin();
-    */
     const editor = new PaintEditor();
 });
 window.addEventListener('beforeunload', (event) => {
